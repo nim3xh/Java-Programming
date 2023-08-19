6faa151cf184fc72e65021f2f94536b2b189cd77
nim3xh <nim3xh@gmail.com>
2023-08-19 11:55:43 +0530

    updated

---
+++ DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes{
+	public static void main(String args[]){
+		String name = "Owadi";
+		int age = 23;
+		double weight = 40.5;
+
+		System.out.println("Name: "+name+"\nAge: "+age+"\nWeight: "+weight);
+	}
+}
